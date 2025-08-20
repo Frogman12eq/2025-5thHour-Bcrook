0921df4715c2f8a6851b63f93868f67bb0de6d5c
Bryson Crook <bc129801@student.wpsok.org>
2025-08-20 13:06:35 -0500

    HW2

---
+++ HW2.py
@@ -0,0 +1,28 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW2
+
+
+#1. Print "Hello World!"
+print("Hello World")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+deadEye = 10
+mask_of_the_child= "Hamburger, cheese burger"
+explosion= True
+#3. Print all three variables on the same print function (at the same time).
+print(deadEye, mask_of_the_child, explosion)
+#4. Create a variable that asks the user to input an integer.
+Chicken_Sandwich=(int(input("Insert integer")))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+Hamborger=(deadEye + Chicken_Sandwich)
+print(Hamborger)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(Hamborger/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+explosion=False
+#8. Print the value of the boolean variable.
+print(explosion)
+#9. Create a variable with a number that contains decimals.
+neutronStyle=10000.01
+#10. Round the number from #9 up or down using the round function
+print(round(neutronStyle))
