d159a159d3e654959d79fef2283274f2bce78b7b
Bryson Crook <bc129801@student.wpsok.org>
2025-09-17 12:25:37 -0500

    added playground

---
+++ Playground.py
@@ -0,0 +1,13 @@
+#Name: Bryson Crook
+#Class: 5th hour computer science
+#Assignment: Playground
+
+import random
+guess = input("Enter your guess here range between 1 and 50:")
+number = random.randint(1, 50)
+brysons_guess=(guess)
+print("brysons guess",brysons_guess)
+guess_2 = input("Enter your guess here range between 1 and 50:")
+judes_guess = (guess_2)
+print("Judes guess",judes_guess)
+print(number)
