303e2d102411c048a96f1943f04a9d640d8878d3
Bryson Crook <bc129801@student.wpsok.org>
2025-09-18 13:21:07 -0500

    added playground

--- Playground.py
+++ Playground.py
@@ -3,6 +3,8 @@
 #Assignment: Playground
 
 import random
+
+'''
 guess = input("Enter your guess here range between 1 and 50:")
 number = random.randint(1, 50)
 brysons_guess=(guess)
@@ -10,4 +12,29 @@ print("brysons guess",brysons_guess)
 guess_2 = input("Enter your guess here range between 1 and 50:")
 judes_guess = (guess_2)
 print("Judes guess",judes_guess)
+guess_3 = input("Enter your guess here range between 1 and 50:")
+ivan_guess=(guess_3)
+print("ivan guess",ivan_guess)
 print(number)
+'''
+
+Character_name= input("Enter character name: ")
+print(Character_name)
+character_height=input("enter height here: ")
+print(character_height)
+character_weight=input("enter weight here: ")
+print(character_weight)
+character_weapon=input("Weapon: ")
+print(character_weapon)
+hair_color= input("Enter hair color: ")
+print(hair_color)
+eye_color= input("Enter eye color: ")
+print(eye_color)
+character_skintone= input("Enter character skin tone: ")
+print(character_skintone)
+character_race= input("Enter character race: ")
+print(character_race)
+character_age= input("Enter character age: ")
+print(character_age)
+character_backstory= input("Enter character backstory: ")
+print(character_backstory)
