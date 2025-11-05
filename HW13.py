c499896f15a23e8c8c61505d18d00f396998b6ac
Bryson Crook <bc129801@student.wpsok.org>
2025-11-05 13:15:36 -0600

    added capital to Fizz, Fizzbuzz, and Buzz

--- HW13.py
+++ HW13.py
@@ -12,11 +12,11 @@
 i = 1
 while i <= 100:
     if  i % 3 == 0 and i % 5 == 0:
-        (print("fizzbuzz"))
+        print("Fizzbuzz")
     elif i % 3 == 0:
-        print("fizz")
+        print("Fizz")
     elif i % 5 == 0:
-        print("buzz")
+        print("Buzz")
     else:
         print(i)
 
