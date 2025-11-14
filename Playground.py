a6baa5f8899bc38ac060aba5990fc86f78077acd
Bryson Crook <bc129801@student.wpsok.org>
2025-11-14 12:59:08 -0600

    Added HW14

--- Playground.py
+++ Playground.py
@@ -17,4 +17,33 @@ ivan_guess=(guess_3)
 print("ivan guess",ivan_guess)
 print(number)
 '''
+'''
+left_right = random.choice([ "Right", "Left"])
+direction = random.choice(["forward", "backwards"])
+key_word_guess= input("Enter left or right here:")
+if key_word_guess == left_right:
+    direction_of_choice = input("Enter forward or backwards here:")
+    if direction_of_choice == direction:
+        print("You live")
+else:
+    print("You Die")
+    '''
+zombie_health= 27
+zombie_damage= 2
+zombie= [zombie_health,zombie_damage ]
+print(zombie)
+player_health= 50
+player_damage= 4
+player= [player_health, player_damage]
+print(player)
+zombie_location = random.choice(["Zombie", "No Zombie"])
+left_right= input("Left or Right?")
+if left_right == "Left":
+    print("you went left")
+    if zombie_location == "Zombie":
+        print("Enemy ahead")
+
+
+
+
 
